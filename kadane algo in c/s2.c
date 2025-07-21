e930a05d9852108bfa0fe11bc4cec7c623eb0d5e
deepak0331 <jain1227672@gmail.com>
2025-07-21 16:24:29 +0530

    token optimization remaining

--- kadane algo in c/s2.c
+++ kadane algo in c/s2.c
@@ -29,3 +29,49 @@ int main() {
 
     return 0;
 }
+int dynamicProgramming(int* nums, int numsSize) {
+    int max_sum = INT_MIN;
+    int current_sum = 0;
+
+    for (int i = 0; i < numsSize; ++i) {
+        current_sum += nums[i];
+        if (current_sum > max_sum) {
+            max_sum = current_sum;
+        }
+        if (current_sum < 0) {
+            current_sum = 0;
+        }
+    }
+
+    return max_sum;
+}
+int divideAndConquer(int* nums, int left, int right) {
+    if (left > right) return INT_MIN;
+
+    if (left == right) return nums[left];
+
+    int mid = (left + right) / 2;
+
+    int left_max = divideAndConquer(nums, left, mid - 1);
+    int right_max = divideAndConquer(nums, mid + 1, right);
+
+    int cross_max = nums[mid];
+    int temp_sum = 0;
+
+    for (int i = mid - 1; i >= left; --i) {
+        temp_sum += nums[i];
+        if (temp_sum > cross_max) {
+            cross_max = temp_sum;
+        }
+    }
+
+    temp_sum = 0;
+    for (int i = mid + 1; i <= right; ++i) {
+        temp_sum += nums[i];
+        if (temp_sum > cross_max) {
+            cross_max += temp_sum;
+        }
+    }
+
+    return max(max(left_max, right_max), cross_max);
+}
